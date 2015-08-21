f9ac48246ec272fb3c46b3a3b72eb0fb3750f8f0
松岡 利昌 <matsuoka@entap.co.jp>
2015-08-21 23:28:24 +0900

    Copyrightの年を変更

--- system/photon_config.php
+++ system/photon_config.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright (c) 2014, Entap,Inc.
+ * Copyright (c) 2015, Entap,Inc.
  * All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without
